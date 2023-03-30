612e487c80926667aefb1baf5fc29f8a4e6e68af
Roddy Andrade <70487872+Seerendo@users.noreply.github.com>
2023-03-30 14:43:43 -0500

    Cambio en la ruta ParamById

--- infraestructure/routes/param.routes.js
+++ infraestructure/routes/param.routes.js
@@ -4,7 +4,7 @@ module.exports = function ({ ParamController }) {
     const router = Router();
 
     router.get('/', ParamController.getParams.bind(ParamController));
-    router.get('/:id', ParamController.getParamById.bind(ParamController));
+    router.get('/byId', ParamController.getParamById.bind(ParamController));
     
     return router;
 }
